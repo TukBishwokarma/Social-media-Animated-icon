e2df8e9d227b8a00e34f7a355ed00e7feb74b199
Tuk <tukbishwokarma@gmail.com>
2024-08-28 12:53:49 +0545

    first

---
+++ script.js
@@ -0,0 +1,20 @@
+const counters = document.querySelectorAll('.counter');
+
+counters.forEach(counter => {
+    const updateCount = () => {
+        const target = +counter.getAttribute('data-target');
+        const count = +counter.innerText;
+
+        const speed = 1000; // Adjust speed here
+        const increment = target / speed;
+
+        if (count < target) {
+            counter.innerText = Math.ceil(count + increment);
+            setTimeout(updateCount, 10);
+        } else {
+            counter.innerText = target;
+        }
+    };
+
+    updateCount();
+});
